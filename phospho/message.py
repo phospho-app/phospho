c3d956f120c101ac7873cac46500a75bb43f9fd9
Pierre-LouisBJT <78322686+Pierre-LouisBJT@users.noreply.github.com>
2023-09-08 17:15:21 +0200

    Changed message archi

--- phospho/message.py
+++ phospho/message.py
@@ -1,13 +1,10 @@
 """
 Define the message class
 """
-import threading # Forensuring thread safety
-
-
 
 class Message:
     # Typed attributes
-    def __init__(self, content: str, payload: dict, metadata: dict):
+    def __init__(self, content: str, payload: dict, metadata={}):
         self.content = content # can be empty, even though it doesn't make sense
         self.payload = payload # Implemented by the dev user, can be empty
         self.metadata = metadata # timestamp, user_id, etc. TODO: define the metadata
