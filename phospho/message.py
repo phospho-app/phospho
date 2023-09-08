f96bebe786a40ef94448b011d327d43b9367c345
Pierre-LouisBJT <78322686+Pierre-LouisBJT@users.noreply.github.com>
2023-09-08 17:22:26 +0200

    V0_0_5

--- phospho/message.py
+++ phospho/message.py
@@ -4,7 +4,7 @@ Define the message class
 
 class Message:
     # Typed attributes
-    def __init__(self, content: str, payload: dict, metadata={}):
-        self.content = content # can be empty, even though it doesn't make sense
+    def __init__(self, content: str, payload={}, metadata={}):
+        self.content = content # can be empty, even though it doesn't make sense. Cannot be None
         self.payload = payload # Implemented by the dev user, can be empty
         self.metadata = metadata # timestamp, user_id, etc. TODO: define the metadata
