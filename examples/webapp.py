5f531babc4b1942a912278ce4ba70f53852420ee
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2023-11-27 10:36:35 +0100

    Adds example webapp

---
+++ examples/webapp.py
@@ -0,0 +1,56 @@
+"""
+Streamlit webapp with an OpenAI chatbot
+
+# Running
+
+1. Create a secrets file examples/.streamlit/secrets.toml with your OpenAI API key
+```
+OPENAI_API_KEY="sk-..." # your actual key
+```
+
+2. Launch the webapp
+```
+cd examples/ 
+streamlit run webapp.py
+```
+
+"""
+
+import streamlit as st
+from openai import OpenAI
+
+st.title("ChatGPT-like clone")
+
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.content or ""
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
