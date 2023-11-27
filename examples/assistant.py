19fe2406f939f14a324ff92e6ed982315323d95a
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2023-11-27 17:13:33 +0100

    Improve wording

--- examples/assistant.py
+++ examples/assistant.py
@@ -25,9 +25,9 @@ from openai.types.chat import ChatCompletionChunk
 from openai._streaming import Stream
 
 
-st.title("ChatGPT-like clone")
+st.title("Assistant")  # Let's do an LLM-powered assistant !
 
-# Initialize phospho
+# Initialize phospho to collect logs
 phospho.init(
     api_key=st.secrets["PHOSPHO_API_KEY"],
     project_id=st.secrets["PHOSPHO_PROJECT_ID"],
@@ -35,32 +35,32 @@ phospho.init(
 )
 phospho.config.BASE_URL = "https://phospho-backend-zxs3h5fuba-ew.a.run.app/v0"
 
-# Initialize the LLM provider
+# We will use OpenAI
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+# The messages between user and assistant are kept in the session_state (the browser's cache)
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
+# Messages are displayed the following way
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(name=message["role"]):
         st.markdown(message["content"])
 
+# This is the user's textbox for chatting with the assistant
 if prompt := st.chat_input("What is up?"):
+    # When the user sends a message...
     new_message = {"role": "user", "content": prompt}
     st.session_state.messages.append(new_message)
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    # ... the assistant replies
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_str_response = ""
-        # Construct a query to OpenAI
+        # We build a query to OpenAI
         full_prompt = {
-            # model is the OpenAI model we use, eg. "gpt-3.5-turbo"
-            "model": st.session_state["openai_model"],
+            "model": "gpt-3.5-turbo",
             # messages contains the whole chat history
             "messages": [
                 {"role": m["role"], "content": m["content"]}
@@ -69,25 +69,29 @@ if prompt := st.chat_input("What is up?"):
             # stream asks to return a Stream object
             "stream": True,
         }
-        # Call OpenAI API and get a stream
+        # The OpenAI module gives us back a stream object
         streaming_response: Stream[
             ChatCompletionChunk
         ] = client.chat.completions.create(**full_prompt)
-        # If we iterate on streaming_response, we get a token by token response
+        # When you iterate on the stream, you get a token for every response
         for response in streaming_response:
             # We can log each individual response in phospho
+            # phospho takes care of aggregating all of the tokens into a single, sensible log.
+            # It's battery-included.
+            # phospho will log all the parameters and all the responses, which is great for debugging
             logged_content = phospho.log(input=full_prompt, output=response)
+
             # We ask streamlit to display the output of the logged_content.
-            # phospho takes care of logging, extracting and aggregating all the chunked outputs
-            # The displayed output is equivalent to:
+            # This is equivalent to:
             # full_str_response += response.choices[0].delta.content or ""
             full_str_response = logged_content["output"] or ""
 
             message_placeholder.markdown(full_str_response + "▌")
 
-        # If you don't want to log every streaming chunk, you can also
-        # just logging the output text to phospho
-        # phospho.log(input=full_prompt, output=full_str_response)
+        # If you don't want to log every streaming chunk (too much data ?)
+        # you can simply log the final output.
+        # phospho logging is meant to be flexible.
+        # phospho.log(input=full_prompt, output=full_str_response, metadata={"stuff": "other"})
         message_placeholder.markdown(full_str_response)
 
     st.session_state.messages.append(
