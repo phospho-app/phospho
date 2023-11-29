9072bd1190087cdee810e494c925682aa317efe0
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2023-11-29 19:35:27 +0100

    Update the historical api and tests to remove deprecated endpoints

--- phospho/tasks.py
+++ phospho/tasks.py
@@ -22,7 +22,7 @@ class Task:
         """
         if self._content is None:
             # Query the server
-            response = self._client._get(f"/tasks/{self._task_id}/")
+            response = self._client._get(f"/tasks/{self._task_id}")
             self._content = response.json()
 
         return self._content
@@ -32,42 +32,40 @@ class Task:
         Refresh the content of the task from the server
         Done inplace
         """
-        response = self._client._get(f"/tasks/{self._task_id}/")
+        response = self._client._get(f"/tasks/{self._task_id}")
         self._content = response.json()
 
-    def update(self, metadata: Optional[dict] = None, data: Optional[dict] = None):
-        if metadata is None and data is None:
-            raise ValueError(
-                "You must provide either metadata or data to update a task"
-            )
+    # def update(self, metadata: Optional[dict] = None, data: Optional[dict] = None):
+    #     if metadata is None and data is None:
+    #         raise ValueError(
+    #             "You must provide either metadata or data to update a task"
+    #         )
 
-        payload = {
-            "metadata": metadata or {},
-            "data": data or {},
-        }
+    #     payload = {
+    #         "metadata": metadata or {},
+    #         "data": data or {},
+    #     }
 
-        response = self._client._post(
-            f"/tasks/{self._task_id}/update/", payload=payload
-        )
+    #     response = self._client._post(f"/tasks/{self._task_id}/update", payload=payload)
 
-        return Task(self._client, response.json()["task_id"])
+    #     return Task(self._client, response.json()["task_id"])
 
     # List steps
-    def list_steps(self):
-        """
-        Use a Generator? -> would enable streaming
-        TODO : add filters, limits and pagination
-        """
-        response = self._client._get(f"/tasks/{self._task_id}/steps/")
+    # def list_steps(self):
+    #     """
+    #     Use a Generator? -> would enable streaming
+    #     TODO : add filters, limits and pagination
+    #     """
+    #     response = self._client._get(f"/tasks/{self._task_id}/steps")
 
-        steps_list = []
+    #     steps_list = []
 
-        for step_content in response.json()["steps"]:
-            steps_list.append(
-                Step(self._client, step_content["step_id"], _content=step_content)
-            )
+    #     for step_content in response.json()["steps"]:
+    #         steps_list.append(
+    #             Step(self._client, step_content["step_id"], _content=step_content)
+    #         )
 
-        return steps_list
+    #     return steps_list
 
 
 class TaskCollection(Collection):
@@ -75,20 +73,19 @@ class TaskCollection(Collection):
     def get(self, task_id: str):
         # TODO: add filters, limits and pagination
 
-        response = self._client._get(f"/tasks/{task_id}/")
+        response = self._client._get(f"/tasks/{task_id}")
 
         return Task(self._client, response.json()["id"], _content=response.json())
 
-    # Get all sessions (filters can be applied) -> projects
-
-    # Create a session
-    # TODO : return a session object, like what replicates does for predictions
+    # Create a task
     def create(
         self,
         session_id: str,
         sender_id: str,
         input: str,
+        output: str,
         additional_input: Optional[dict] = None,
+        additional_output: Optional[dict] = None,
         data: Optional[dict] = None,
     ):
         payload = {
@@ -96,12 +93,14 @@ class TaskCollection(Collection):
             "sender_id": sender_id,
             "input": input,
             "additional_input": additional_input or {},
+            "output": output,
+            "additional_output": additional_input or {},
             "data": data or {},
         }
 
-        response = self._client._post(f"/tasks/", payload=payload)
+        response = self._client._post(f"/tasks", payload=payload)
 
-        return Task(self._client, response.json()["task_id"])
+        return Task(self._client, response.json()["id"])
 
 
 # Get all tasks (filters can be applied)
--- tests/test_client.py
+++ tests/test_client.py
@@ -30,36 +30,36 @@ def test_client_sessions(client_instance):
     assert session_bis.content == session.content, "Session content not equal"
 
     # Update a session
-    new_content = {"name": "test session 2", "test": "test"}
-    session.update(data=new_content)
+    # new_content = {"name": "test session 2", "test": "test"}
+    # session.update(data=new_content)
 
-    session.refresh()
+    # session.refresh()
 
-    assert session.content["data"] == new_content, "Session content not equal"
-
-    # Update a session
-    new_content = {"name": "test session 2", "test": "test"}
-    session.update(metadata={"flag": "success"})
-
-    session.refresh()
-    # Check the metadata contains the flag
-    assert (
-        session.content["metadata"]["flag"] == "success"
-    ), "Session metadata not equal"
+    # assert session.content["data"] == new_content, "Session content not equal"
 
     # Update a session
-    session.update(metadata={"flag": "success"}, data=new_content)
-    # Check the data and the metadata contains the flag
-    assert (
-        session.content["metadata"]["flag"] == "success"
-    ), "Session metadata not equal"
-    assert session.content["data"] == new_content, "Session content not equal"
+    # new_content = {"name": "test session 2", "test": "test"}
+    # session.update(metadata={"flag": "success"})
 
-    # Update
+    # session.refresh()
+    # # Check the metadata contains the flag
+    # assert (
+    #     session.content["metadata"]["flag"] == "success"
+    # ), "Session metadata not equal"
 
-    session.refresh()
+    # # Update a session
+    # session.update(metadata={"flag": "success"}, data=new_content)
+    # # Check the data and the metadata contains the flag
+    # assert (
+    #     session.content["metadata"]["flag"] == "success"
+    # ), "Session metadata not equal"
+    # assert session.content["data"] == new_content, "Session content not equal"
 
-    assert session.content["data"] == new_content, "Session content not equal"
+    # # Update
+
+    # session.refresh()
+
+    # assert session.content["data"] == new_content, "Session content not equal"
 
     # Tasks
 
@@ -69,7 +69,7 @@ def test_client_sessions(client_instance):
 
     # Create a Task
     task = client_instance.tasks.create(
-        session_id=session.id, sender_id="sender_id", input="input"
+        session_id=session.id, sender_id="sender_id", input="input", output="output"
     )
 
     # Check if task is created successfully
@@ -87,20 +87,20 @@ def test_client_sessions(client_instance):
     )
 
     # Update the task
-    updated_task = task.update(data={"new_data": "updated_data"})
+    # updated_task = task.update(data={"new_data": "updated_data"})
 
     # Check if the updated task's content matches the expected content
-    assert updated_task.content["data"]["new_data"] == "updated_data"
+    # assert updated_task.content["data"]["new_data"] == "updated_data"
 
     # List steps for the task
-    steps = task.list_steps()
+    # steps = task.list_steps()
 
     # Check if steps is a list of Step objects
-    assert all(isinstance(step, Step) for step in steps)
+    # assert all(isinstance(step, Step) for step in steps)
 
     # Create a Task using the TaskCollection
     task = client_instance.tasks.create(
-        session_id=session.id, sender_id="sender_id", input="input"
+        session_id=session.id, sender_id="sender_id", input="input", output="output"
     )
 
     # Check if task is created successfully
@@ -117,27 +117,27 @@ def test_client_sessions(client_instance):
     ### STEPS ###
 
     # Create a Step
-    step = client_instance.steps.create(
-        task_id=task.id, input="input", name="name", status="status", is_last=True
-    )
+    # step = client_instance.steps.create(
+    #     task_id=task.id, input="input", name="name", status="status", is_last=True
+    # )
 
-    # Check if step is created successfully
-    assert isinstance(step, Step)
+    # # Check if step is created successfully
+    # assert isinstance(step, Step)
 
-    # Refresh the step
-    step.refresh()
+    # # Refresh the step
+    # step.refresh()
 
-    # Get the step
-    retrieved_step = client_instance.steps.get(step.id)
+    # # Get the step
+    # retrieved_step = client_instance.steps.get(step.id)
 
-    print("retrieved step content :", retrieved_step.content)
+    # print("retrieved step content :", retrieved_step.content)
 
-    # Check if the retrieved step's content matches the created step's content
-    assert json.dumps(retrieved_step.content, sort_keys=True) == json.dumps(
-        step.content, sort_keys=True
-    )
+    # # Check if the retrieved step's content matches the created step's content
+    # assert json.dumps(retrieved_step.content, sort_keys=True) == json.dumps(
+    #     step.content, sort_keys=True
+    # )
 
-    # Update the step
-    updated_step = step.update(data={"key": "value"})
+    # # Update the step
+    # updated_step = step.update(data={"key": "value"})
 
-    print(updated_step.content)
+    # print(updated_step.content)
