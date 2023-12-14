93cdcd69fbb7048511878e6205b742bed4a387be
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2023-12-14 15:00:34 +0100

    Change the BASE URL

--- phospho/config.py
+++ phospho/config.py
@@ -2,5 +2,4 @@
 Phospho internal configuration file
 """
 
-# BASE_URL = "http://localhost:8000/v0"
-BASE_URL = "https://phospho-backend-zxs3h5fuba-ew.a.run.app/v0"
+BASE_URL = "https://api.phospho.ai/v0"
