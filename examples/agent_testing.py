0ee9196260fbbd2a16e71844225a1a43df32f259
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2023-12-14 12:30:05 +0100

    Adds call to backend for comparison

--- examples/agent_testing.py
+++ examples/agent_testing.py
@@ -10,34 +10,26 @@ from openai import OpenAI
 from collections import defaultdict
 from pprint import pprint
 from types import GeneratorType
-from typing import Any, Dict
+from typing import Any, Dict, List
 from random import sample
 
 
-# Import the agent you want to test
+# This is the agent to test
 from streamlit_santa_agent.backend import SantaClausAgent
 
 logger = logging.getLogger(__name__)
 
-# Initialize phospho in backtest mode
-os.environ["PHOSPHO_EXECUTION_MODE"] = "backtest"
-phospho.init(project_id="jRg9zVIXRTqmokv84wSt")
 
-# Collect evaluation
-EVALUATION_RESULTS: Dict[str, int] = defaultdict(int)
-COMPARISONS = []
+def get_output_from_agent(input: Dict[str, Any]) -> str:
+    """
+    This function will return the output of the agent given an input
 
-# We'll do the evaluation with OpenAI
-openai_client = OpenAI()
-
-
-def evaluation_function(
-    context: str,
-    old_output_str: str,
-    new_output: Any,
-):
-    """This is a simple evaluation function that calls OpenAI
-    and asks which answer is better"""
+    TODO: The detection of the functions to call, and how to call them
+    (ex: instatiate object, stream outputs, use async, casting inputs, etc.)
+    should be done automatically by the phospho module.
+    """
+    santa_claus_agent = SantaClausAgent()
+    new_output = santa_claus_agent.answer(**input)
 
     # If generator, we need to extract the str from the output
     if isinstance(new_output, GeneratorType):
@@ -48,60 +40,55 @@ def evaluation_function(
     else:
         new_output_str = phospho.extractor.detect_str_from_output(new_output)
 
-    COMPARISONS.append({"input": context, "old": old_output_str, "new": new_output_str})
-
-    prompt = f"""Chose which of the response is better as an answer to the following context:
----
-Context: {context}
----
-Response A: {old_output_str}
----
-Response B: {new_output_str}
-----
-
-Possible answers:
-Choice A: Response A is better
-Choice B: Response B is better
-Choice C: Both are equally good
-Choice D: Both are equally bad
-
-Choice: """
-
-    response = openai_client.chat.completions.create(
-        messages=[{"role": "system", "content": prompt}],
-        model="gpt-3.5-turbo",
-        max_tokens=2,
-    )
-    text_response = response.choices[0].message.content
-    # Map back 'Choice A' to 'Response A is better' etc.
-    mapping = {
-        "Choice A": "Old output is better",
-        "Choice B": "New output is better",
-        "Choice C": "Same quality",
-        "Choice D": "Both are bad",
-    }
-
-    EVALUATION_RESULTS[mapping[text_response]] += 1
+    return new_output_str
 
 
 def evaluate_a_task(task):
-    """This function evaluates a single task"""
+    """This function evaluates a single task using the phospho backend"""
+    global EVALUATION_RESULTS
+    global COMPARISONS
 
     try:
         print("Task id: ", task.id)
-        santa_claus_agent = SantaClausAgent()
-        new_output = santa_claus_agent.answer(**task.content["additional_input"])
-        # Compare
-        evaluation_function(
+        new_output_str = get_output_from_agent(task.content["additional_input"])
+
+        #
+        comparison_result = phospho.client.compare(
             task.content["input"],
             task.content["output"],
-            new_output,
+            new_output_str,
         )
+
+        # Collect the results
+        COMPARISONS.append(
+            {
+                "input": task.content["input"],
+                "old": task.content["output"],
+                "new": new_output_str,
+            }
+        )
+        EVALUATION_RESULTS[comparison_result.comparison_result] += 1
+
     except Exception as e:
         logger.error(f"Error while answering task {task.id}: {e}")
 
 
+# We'll collect evaluation in dict
+EVALUATION_RESULTS: Dict[str, int] = defaultdict(int)
+COMPARISONS: List[dict] = []
+
+
 def main():
+    """
+    Backtesting: This function pull all the tasks logged to phospho and run the agent on them.
+
+    TODO: This should be abstracted. The user should be able to run this with a single command
+    and basic filters, such as the agent to test, the project fetch tasks from, the time range, etc.
+    """
+    # Initialize phospho in backtest mode
+    os.environ["PHOSPHO_EXECUTION_MODE"] = "backtest"
+    phospho.init(project_id="jRg9zVIXRTqmokv84wSt")
+
     # Pull the logs from phospho
     tasks = phospho.client.tasks.get_all()
     if len(tasks) > 10:
@@ -111,6 +98,7 @@ def main():
         # Submit tasks to the executor
         executor.map(evaluate_a_task, tasks)
 
+    # Display a summary of the results
     pprint(COMPARISONS)
     print(EVALUATION_RESULTS)
 
