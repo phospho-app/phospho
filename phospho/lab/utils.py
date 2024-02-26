209528f325003b343e2fb97996f69777276e4b1f
Pierre-LouisBJT <plbiojout@gmail.com>
2024-02-26 15:40:36 -0800

    Add an ignore of the default config

--- phospho/lab/utils.py
+++ phospho/lab/utils.py
@@ -23,15 +23,25 @@ def generate_configurations(
     :param job_config: The job config to generate the configurations from
     :param exclude_default: Whether to exclude the default configuration
     """
-    # Get all possible values for the myConfig model
+    # Get all possible values for the JobConfig model
     literal_values = get_literal_values(job_config)
 
     # Generate all possible combinations of Literal values
-    combinations = list(itertools.product(*literal_values.values()))
+    all_combinations = list(itertools.product(*literal_values.values()))
 
-    # Create a list of myConfig objects with all possible combinations
+    # Exclude the combination that matches the default configuration
+    if exclude_default:
+        default_values = tuple(
+            getattr(job_config, field) for field in literal_values.keys()
+        )
+        combinations = [combo for combo in all_combinations if combo != default_values]
+    else:
+        combinations = all_combinations
+
+    # Create a list of JobConfig objects with all possible combinations
     config_objects = [
-        job_config(**dict(zip(literal_values.keys(), combo))) for combo in combinations
+        job_config.__class__(**dict(zip(literal_values.keys(), combo)))
+        for combo in combinations
     ]
 
     return config_objects
