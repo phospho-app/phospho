07d96c24e8a853434b52dd88d6f403010268efbb
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-03-01 08:42:59 -0800

    Refacto models to be async first

--- phospho-python/phospho/lab/language_models.py
+++ phospho-python/phospho/lab/language_models.py
@@ -1,186 +1,44 @@
-from abc import ABC, abstractmethod
-from phospho import config
-
-import openai
-import ollama
-import mistralai
-from mistralai.client import MistralClient
+import os
+from typing import Tuple
+from openai import AsyncOpenAI, OpenAI
 
 
-class BaseLanguageModel(ABC):
-    @abstractmethod
-    def invoke(self, *args, **kwargs) -> str:
-        pass
-
-
-class OpenAIModel(BaseLanguageModel):
+def get_provider_and_model(model: str) -> Tuple[str, str]:
     """
-    A class to interact with the OpenAI API.
+    Get the provider and model from a string in the format "provider:model"
+    If no provider is specified, it defaults to "openai"
+
+    Args:
+        model (str): The model string in the format "provider:model"
+
+    Returns:
+        tuple: A tuple with the provider and model
     """
+    split_result = model.split(":")
+    if len(split_result) == 1:
+        return "openai", split_result[0]
+    return split_result[0], split_result[1]
 
-    def __init__(
-        self,
-        api_key: str,
-        model_name: str,
-        temperature: float = 0.0,
-        max_tokens: int = 1,
-    ):
-        self.model_name = model_name
-        self.temperature = temperature
-        self.max_tokens = max_tokens
 
-        # Create the async client
-        self._client = openai.Client(api_key=api_key)
-
-    def invoke(self, prompt) -> str:
-        response = self._client.chat.completions.create(
-            model=self.model_name,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt},
-            ],
-            max_tokens=self.max_tokens,
-            temperature=self.temperature,
+def get_async_client(provider: str) -> AsyncOpenAI:
+    if provider == "openai":
+        return AsyncOpenAI()
+    if provider == "mistral":
+        return AsyncOpenAI(
+            base_url="https://api.mistral.ai/v1/", api_key=os.getenv("MISTRAL_API_KEY")
         )
-        return response.choices[0].message.content
+    if provider == "ollama":
+        return AsyncOpenAI(base_url="http://localhost:11434/v1/")
+    raise NotImplementedError(f"Provider {provider} is not supported.")
 
 
-class AsyncOpenAIModel(BaseLanguageModel):
-    """
-    A class to interact with the OpenAI API.
-    """
-
-    def __init__(
-        self,
-        api_key: str,
-        model_name: str,
-        temperature: float = 0.0,
-        max_tokens: int = 1,
-    ):
-        self.model_name = model_name
-        self.temperature = temperature
-        self.max_tokens = max_tokens
-
-        # Create the async client
-        self._async_client = openai.AsyncClient(api_key=api_key)
-
-    async def invoke(self, prompt) -> str:
-        response = await self._async_client.chat.completions.create(
-            model=self.model_name,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt},
-            ],
-            max_tokens=1,
-            temperature=0,
+def get_sync_client(provider: str) -> OpenAI:
+    if provider == "openai":
+        return OpenAI()
+    if provider == "mistral":
+        return OpenAI(
+            base_url="https://api.mistral.ai/v1/", api_key=os.getenv("MISTRAL_API_KEY")
         )
-        return response.choices[0].message.content
-
-
-class OllamaModel(BaseLanguageModel):
-    """
-    A class to interact with the Ollama API of a model running locally.
-    We have it return only one token.
-    """
-
-    def __init__(
-        self,
-        model_name: str,
-    ):
-        self.model_name = model_name
-
-    def invoke(self, prompt) -> str:
-        stream = ollama.chat(
-            model=self.model_name,
-            messages=[{"role": "user", "content": prompt}],
-            stream=True,
-        )
-
-        for chunk in stream:
-            return chunk["message"]["content"]
-
-
-class MistralModel(BaseLanguageModel):
-    """
-    A class to interact with the Mistral API.
-    """
-
-    def __init__(
-        self,
-        api_key: str,
-        model_name: str,
-        temperature: float = 0.0,
-        max_tokens: int = 1,
-    ):
-        self.model_name = model_name
-        self.temperature = temperature
-        self.max_tokens = max_tokens
-
-        # Create the async client
-        self._client = MistralClient(api_key=api_key)
-
-    def invoke(self, prompt) -> str:
-        messages = [
-            mistralai.models.chat_completion.ChatMessage(role="user", content=prompt)
-        ]
-
-        # No streaming
-        chat_response = self._client.chat(
-            model=self.model_name,
-            messages=messages,
-            temperature=self.temperature,
-            max_tokens=self.max_tokens,
-        )
-
-        return chat_response.choices[0].message.content
-
-
-def language_model_from_id(model_id: str) -> BaseLanguageModel or None:
-    """
-    Mapping of model id to language model object.
-    Return a language model object from an ID.
-    """
-    # OpenAI API
-    if model_id == "openai:gpt-3.5-turbo":
-        return OpenAIModel(
-            api_key=config.OPENAI_API_KEY,
-            model_name="gpt-3.5-turbo",
-        )
-    if model_id == "openai:gpt-4":
-        return OpenAIModel(
-            api_key=config.OPENAI_API_KEY,
-            model_name="gpt-4",
-        )
-    # Ollama local API
-    if model_id == "ollama:mistral-7B":
-        return OllamaModel(
-            model_name="mistral",
-        )
-    # Mistral AI API
-    if model_id == "mistral:open-mistral-7b":
-        return MistralModel(
-            api_key=config.MISTRAL_API_KEY,
-            model_name="open-mistral-7b",
-        )
-    if model_id == "mistral:open-mixtral-8x7b":
-        return MistralModel(
-            api_key=config.MISTRAL_API_KEY,
-            model_name="open-mixtral-8x7b",
-        )
-    if model_id == "mistral:mistral-small-latest":
-        return MistralModel(
-            api_key=config.MISTRAL_API_KEY,
-            model_name="mistral-small-latest",
-        )
-    if model_id == "mistral:mistral-medium-latest":
-        return MistralModel(
-            api_key=config.MISTRAL_API_KEY,
-            model_name="mistral-medium-latest",
-        )
-    if model_id == "mistral:mistral-large-latest":
-        return MistralModel(
-            api_key=config.MISTRAL_API_KEY,
-            model_name="mistral-large-latest",
-        )
-
-    return None
+    if provider == "ollama":
+        return OpenAI(base_url="http://localhost:11434/v1/")
+    raise NotImplementedError(f"Provider {provider} is not supported.")
