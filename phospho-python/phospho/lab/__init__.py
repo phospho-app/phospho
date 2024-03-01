c1324eb5e60f033956795bb569017f7ebb0ff997
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-02-29 16:20:58 -0800

    Rewrite quickstart

--- phospho-python/phospho/lab/__init__.py
+++ phospho-python/phospho/lab/__init__.py
@@ -1,3 +1,3 @@
 from .lab import Workload, Job
-from .models import JobResult, Message, JobConfig
+from .models import JobResult, Message, JobConfig, EventConfig
 from . import job_library as job_library
