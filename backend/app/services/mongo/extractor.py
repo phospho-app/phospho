4c2f6fc2d03092814ecb899228e93bbcbe47abd0
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-03-04 15:59:12 -0800

    Adds main pipeline and qdrant env

--- backend/app/services/mongo/extractor.py
+++ backend/app/services/mongo/extractor.py
@@ -8,6 +8,7 @@ from typing import List
 import httpx
 from app.api.v2.models import LogEvent
 from app.core import config
+from app.db.models import Task
 from app.services.slack import slack_notification
 from app.utils import generate_uuid
 from loguru import logger
@@ -53,6 +54,49 @@ async def run_log_process(
                     "logs_to_process": logs_to_process,
                     "project_id": project_id,
                     "org_id": org_id,
+                },
+                headers={
+                    "Authorization": f"Bearer {config.EXTRACTOR_SECRET_KEY}",
+                    "Content-Type": "application/json",
+                },
+                timeout=60,
+            )
+            if response.status_code != 200:
+                logger.error(
+                    f"Error returned when calling main pipeline (status code: {response.status_code}): {response.text}"
+                )
+                # If we are in production, send a Slack message
+                if config.ENVIRONMENT == "production":
+                    await slack_notification(
+                        f"Error returned when calling main pipeline (status code: {response.status_code}): {response.text}"
+                    )
+        except Exception as e:
+            errror_id = generate_uuid()
+            error_message = f"Caught error while calling main pipeline (error_id: {errror_id}): {e}\n{traceback.format_exception(e)}"
+            logger.error(error_message)
+
+            traceback.print_exc()
+            if config.ENVIRONMENT == "production":
+                if len(error_message) > 200:
+                    slack_message = error_message[:200]
+                else:
+                    slack_message = error_message
+                await slack_notification(slack_message)
+
+
+async def run_main_pipeline(task: Task):
+    """
+    Run the log procesing pipeline on a task asynchronously
+    """
+    async with httpx.AsyncClient() as client:
+        logger.debug(
+            f"Calling the extractor API for task {task.id} logevents: {config.EXTRACTOR_URL}/v1/pipelines/log"
+        )
+        try:
+            response = await client.post(
+                f"{config.EXTRACTOR_URL}/v1/pipelines/main",  # WARNING: hardcoded API version
+                json={
+                    "task": task.model_dump(),
                 },
                 headers={
                     "Authorization": f"Bearer {config.EXTRACTOR_SECRET_KEY}",
