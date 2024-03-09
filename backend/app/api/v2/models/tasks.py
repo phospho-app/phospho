04f2dd8cbf356b087669e007c1ea48b6977f7c12
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-03-08 18:07:02 -0800

    Adds model to API output

--- backend/app/api/v2/models/tasks.py
+++ backend/app/api/v2/models/tasks.py
@@ -41,7 +41,16 @@ class TaskUpdateRequest(BaseModel):
 
 class FlattenedTask(BaseModel, extra="allow"):
     task_id: str
+    task_input: Optional[str] = None
+    task_output: Optional[str] = None
     task_metadata: Optional[dict] = None
+    task_eval: Optional[Literal["success", "failure"]] = None
+    task_eval_source: Optional[str] = None
+    task_eval_at: Optional[int] = None
+    task_created_at: Optional[int] = None
+    session_id: Optional[str] = None
+    event_name: Optional[str] = None
+    event_created_at: Optional[int] = None
 
 
 class FlattenedTasks(BaseModel):
