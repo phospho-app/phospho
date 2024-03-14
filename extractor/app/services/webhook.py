9e0cf97eea9c038c4727ee189406d0d9aa8fe0cb
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-03-14 15:24:08 -0700

    Add webhook support and job metadata

--- extractor/app/services/webhook.py
+++ extractor/app/services/webhook.py
@@ -1,32 +1,36 @@
 from typing import Optional
 
-import requests
+import aiohttp
 from loguru import logger
 
 
-def trigger_webhook(
-    url: str, json: dict, timeout: int = 1, headers: Optional[dict] = None
+async def trigger_webhook(
+    url: str, json: dict, timeout: int = 3, headers: Optional[dict] = None
 ):
     """
-    Function to trigger a webhook.
+    Async function to trigger a webhook. Sends a POST request to the given URL
+    with the given data.
+
     :param url: The URL to trigger the webhook on.
     :param data: The data to send to the webhook.
     :param timeout: The timeout for the request, an int in seconds.
     """
     # Filter empty values from the headers (where str is "")
     headers = {k: v for k, v in (headers or {}).items() if v is not None and v != ""}
+
     # If the url is not set, return early
     if url == "":
         logger.warning("No webhook URL set, skipping webhook trigger")
         return
+
     try:
-        response = requests.post(url, json=json, timeout=timeout, headers=headers)
-        response.raise_for_status()
-        logger.info(f"Webhook triggered successfully: {response.status_code}")
-        return response.text
-    except requests.exceptions.Timeout:
-        logger.debug(f"Request timed out when sending webhook to {url}")
-    except requests.exceptions.HTTPError as err:
-        logger.debug(f"HTTP error occurred when webhook {url}: {err}")
-    except requests.exceptions.RequestException as err:
-        logger.debug(f"A request exception occurred when webhook {url}: {err}")
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url, json=json, timeout=timeout, headers=headers
+            ) as response:
+                response.raise_for_status()
+                logger.info(f"Webhook triggered successfully: {response.status}")
+                return await response.text()
+    except aiohttp.ClientError as e:
+        logger.error(f"Error sending webhook to {url}: {e}")
+        return None
