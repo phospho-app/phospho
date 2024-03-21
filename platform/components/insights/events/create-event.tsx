6cb48d2aa472854356f95fd67fde8a2aa9ae6dbb
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-03-21 15:13:28 -0700

    Fix import

--- platform/components/insights/events/create-event.tsx
+++ platform/components/insights/events/create-event.tsx
@@ -133,7 +133,6 @@ export default function CreateEvent() {
                         {...field}
                       />
                     </FormControl>
-
                     <FormMessage />
                   </FormItem>
                 )}
