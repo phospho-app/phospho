6c5ab2abb7f2214752c66cf022c03360ef21ec99
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-03-29 10:17:05 -0700

    Support multi : model names

--- phospho-python/phospho/lab/language_models.py
+++ phospho-python/phospho/lab/language_models.py
@@ -12,7 +12,7 @@ except ImportError:
 def get_provider_and_model(model: str) -> Tuple[str, str]:
     """
     Get the provider and model from a string in the format "provider:model"
-    If no provider is specified, it defaults to "openai"
+    If no provider is specified, it defaults to "openai".
 
     Args:
         model (str): The model string in the format "provider:model"
@@ -28,8 +28,18 @@ def get_provider_and_model(model: str) -> Tuple[str, str]:
 
     split_result = model.split(":")
     if len(split_result) == 1:
-        return "openai", split_result[0]
-    return split_result[0], split_result[1]
+        # Assume default provider to be openai
+        provider = "openai"
+        model_name = split_result[0]
+    elif len(split_result) > 2:
+        # Some model names have :, so we need to join the rest of the string
+        provider = split_result[0]
+        model_name = ":".join(split_result[1:])
+    else:
+        provider = split_result[0]
+        model_name = split_result[1]
+
+    return provider, model_name
 
 
 def get_async_client(provider: str) -> AsyncOpenAI:
