d64042d4011cdd5b3bf768067462997d2e01ebb5
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-04-25 15:12:43 +0200

    Minor edit

--- platform/components/authenticate.tsx
+++ platform/components/authenticate.tsx
@@ -9,7 +9,7 @@ const Authenticate: React.FC = () => {
   return (
     <>
       <div className="hidden"></div>
-      <div className="h-screen container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 lg:mx-0">
+      <div className="h-screen container relative flex-col items-center justify-center md:grid lg:max-w-screen lg:grid-cols-2 lg:px-0 lg:mx-0">
         {/*<Link
               href="/dashboard"
               className={cn(
@@ -33,7 +33,7 @@ const Authenticate: React.FC = () => {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 lg:col-span-1">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] mt-24">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight text-green-500">
