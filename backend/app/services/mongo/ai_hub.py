eb588170be2a1c7524a6998a4ba5c3d0b517cecb
Pierre-LouisBJT <plbiojout@gmail.com>
2024-04-30 10:46:59 +0200

    Made calls to ai hub async

--- backend/app/services/mongo/ai_hub.py
+++ backend/app/services/mongo/ai_hub.py
@@ -29,52 +29,54 @@ def health_check():
         return False
 
 
-def fetch_models(org_id: Optional[str] = None) -> Optional[ModelsResponse]:
+async def fetch_models(org_id: Optional[str] = None) -> Optional[ModelsResponse]:
     """
     List all the models of the AI Hub
     An organization id can be provided to filter the models
     Only the models with the status "trained" are returned
     """
-    try:
-        if org_id is None:
-            response = httpx.get(f"{config.PHOSPHO_AI_HUB_URL}/v1/models")
-        else:
-            response = httpx.get(
-                f"{config.PHOSPHO_AI_HUB_URL}/v1/models",
-                params={"org_id": org_id},
+    async with httpx.AsyncClient() as client:
+        try:
+            if org_id is None:
+                response = client.get(f"{config.PHOSPHO_AI_HUB_URL}/v1/models")
+            else:
+                response = client.get(
+                    f"{config.PHOSPHO_AI_HUB_URL}/v1/models",
+                    params={"org_id": org_id},
+                    headers={
+                        "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_URL}",
+                        "Content-Type": "application/json",
+                    },
+                    timeout=60,
+                )
+            # PArse the response
+            return ModelsResponse(**response.json())
+
+        except Exception as e:
+            logger.error(e)
+            return None
+
+
+async def fetch_model(model_id: str) -> Model | None:
+    """
+    Get a model by its id
+    """
+    async with httpx.AsyncClient() as client:
+        try:
+            response = client.get(
+                f"{config.PHOSPHO_AI_HUB_URL}/v1/models/{model_id}",
                 headers={
-                    "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_URL}",
+                    "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_API_KEY}",
                     "Content-Type": "application/json",
                 },
                 timeout=60,
             )
-        # PArse the response
-        return ModelsResponse(**response.json())
+            # Parse the response
+            return Model(**response.json())
 
-    except Exception as e:
-        logger.error(e)
-        return None
-
-
-def fetch_model(model_id: str) -> Model | None:
-    """
-    Get a model by its id
-    """
-    try:
-        response = httpx.get(
-            f"{config.PHOSPHO_AI_HUB_URL}/v1/models/{model_id}",
-            headers={
-                "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_API_KEY}",
-                "Content-Type": "application/json",
-            },
-            timeout=60,
-        )
-        # Parse the response
-        return Model(**response.json())
-
-    except Exception as e:
-        logger.error(e)
-        return None
+        except Exception as e:
+            logger.error(e)
+            return None
 
 
 async def train_model(request_body: TrainRequest) -> Model | None:
@@ -92,43 +94,45 @@ async def train_model(request_body: TrainRequest) -> Model | None:
     else:
         file_id = request_body.dataset
 
-    try:
-        response = httpx.post(
-            f"{config.PHOSPHO_AI_HUB_URL}/v1/train",
-            json={
-                "model": request_body.model,
-                "dataset": file_id,
-                "task_type": request_body.task_type,
-                "org_id": request_body.org_id,
-            },
-            headers={
-                "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_API_KEY}",
-                "Content-Type": "application/json",
-            },
-            timeout=60,
-        )
-        # Parse the response
-        return Model(**response.json())
+    async with httpx.AsyncClient() as client:
+        try:
+            response = client.post(
+                f"{config.PHOSPHO_AI_HUB_URL}/v1/train",
+                json={
+                    "model": request_body.model,
+                    "dataset": file_id,
+                    "task_type": request_body.task_type,
+                    "org_id": request_body.org_id,
+                },
+                headers={
+                    "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_API_KEY}",
+                    "Content-Type": "application/json",
+                },
+                timeout=60,
+            )
+            # Parse the response
+            return Model(**response.json())
 
-    except Exception as e:
-        logger.error(e)
-        return None
+        except Exception as e:
+            logger.error(e)
+            return None
 
 
-def predict(predict_request: PredictRequest) -> PredictResponse | None:
-    try:
-        response = httpx.post(
-            f"{config.PHOSPHO_AI_HUB_URL}/v1/predict",
-            json=predict_request.model_dump(),
-            headers={
-                "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_API_KEY}",
-                "Content-Type": "application/json",
-            },
-            timeout=60,
-        )
-        # Parse the response
-        return PredictResponse(**response.json())
+async def predict(predict_request: PredictRequest) -> PredictResponse | None:
+    async with httpx.AsyncClient() as client:
+        try:
+            response = client.post(
+                f"{config.PHOSPHO_AI_HUB_URL}/v1/predict",
+                json=predict_request.model_dump(),
+                headers={
+                    "Authorization": f"Bearer {config.PHOSPHO_AI_HUB_API_KEY}",
+                    "Content-Type": "application/json",
+                },
+                timeout=60,
+            )
+            # Parse the response
+            return PredictResponse(**response.json())
 
-    except Exception as e:
-        logger.error(e)
-        return None
+        except Exception as e:
+            logger.error(e)
+            return None
