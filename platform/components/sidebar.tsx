223ba6e2e93f0fdbcf425cc9c95908daf0b16d84
Frédéric Legrand <66426745+fred3105@users.noreply.github.com>
2024-05-16 10:53:00 +0200

    Update sidebar visuals

--- platform/components/sidebar.tsx
+++ platform/components/sidebar.tsx
@@ -3,7 +3,17 @@
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { dataStateStore } from "@/store/store";
-import { Plug, Sparkles, Star, Users } from "lucide-react";
+import {
+  ListChecks,
+  Monitor,
+  Settings,
+  Shuffle,
+  Sparkles,
+  Star,
+  TestTubeDiagonal,
+  TextSearch,
+  Users,
+} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -28,24 +38,7 @@ export function Sidebar() {
               }
               className="w-full justify-start"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="m3 17 2 2 4-4" />
-                <path d="m3 7 2 2 4-4" />
-                <path d="M13 6h8" />
-                <path d="M13 12h8" />
-                <path d="M13 18h8" />
-              </svg>
+              <ListChecks size={16} className="mr-2" />
               Transcripts
             </Button>
           </Link>
@@ -67,22 +60,7 @@ export function Sidebar() {
               }
               className="w-full justify-start"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M21 6H3" />
-                <path d="M10 12H3" />
-                <path d="M10 18H3" />
-                <circle cx="17" cy="15" r="3" />
-                <path d="m21 19-1.9-1.9" />
-              </svg>
+              <TextSearch size={16} className="mr-2" />
               Insights
             </Button>
           </Link>
@@ -94,22 +72,7 @@ export function Sidebar() {
               }
               className="w-full justify-start"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M2 18h1.4c1.3 0 2.5-.6 3.3-1.7l6.1-8.6c.7-1.1 2-1.7 3.3-1.7H22" />
-                <path d="m18 2 4 4-4 4" />
-                <path d="M2 6h1.9c1.5 0 2.9.9 3.6 2.2" />
-                <path d="M22 18h-5.9c-1.3 0-2.6-.7-3.3-1.8l-.5-.8" />
-                <path d="m18 14 4 4-4 4" />
-              </svg>
+              <Shuffle size={16} className="mr-2" />
               AB Testing
             </Button>
           </Link>
@@ -120,22 +83,7 @@ export function Sidebar() {
               }
               className="w-full justify-start"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M21 7 6.82 21.18a2.83 2.83 0 0 1-3.99-.01v0a2.83 2.83 0 0 1 0-4L17 3" />
-                <path d="m16 2 6 6" />
-                <path d="M12 16H4" />
-              </svg>
+              <TestTubeDiagonal size={16} className="mr-2" />
               Tests
             </Button>
           </Link>
@@ -147,19 +95,7 @@ export function Sidebar() {
               }
               className="w-full justify-start"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-                <circle cx="12" cy="12" r="3" />
-              </svg>
+              <Settings size={16} className="mr-2" />
               Settings
             </Button>
           </Link>
