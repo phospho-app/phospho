5de3ff19c35649d13468c79293d04077cdf13fab
Pierre-LouisBJT <plbiojout@gmail.com>
2024-06-07 17:15:53 +0200

    Added token based billing

--- backend/app/api/v2/endpoints/embeddings.py
+++ backend/app/api/v2/endpoints/embeddings.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 
 from app.api.v2.models import Embedding, EmbeddingRequest
 from app.services.mongo.ai_hub import generate_embeddings
+from app.services.mongo.predict import metered_prediction
+from app.core import config
 
 from app.security import authenticate_org_key
 
@@ -18,12 +20,16 @@ async def post_embeddings(
     request_body: EmbeddingRequest,
     org: dict = Depends(authenticate_org_key),
 ):
+    org_id = org["org"]["org_id"]
     # Handle the emebedding model in the request
     if request_body.model != "intent-embed":
         raise HTTPException(
             status_code=400,
             detail="Model not supported. Only 'intent-embed' is supported for now.",
         )
+
+    # Add the organization id to the request body
+    request_body.org_id = org_id
 
     # We assume the model is "phospho-intent-embed"
     embedding = await generate_embeddings(request_body)
@@ -36,5 +42,14 @@ async def post_embeddings(
 
     # Bill the organization for the request
     # background_tasks.add_task(save_embedding, embeddings)
+    if org_id != config.PHOSPHO_ORG_ID:  # and config.ENVIRONMENT == "production":
+        background_tasks.add_task(
+            metered_prediction,
+            org_id=org["org"]["org_id"],
+            model_id=f"phospho:{request_body.model}",
+            inputs=[request_body.text],
+            predictions=[embedding.model_dump()],
+            project_id=request_body.project_id,
+        )
 
     return embedding
