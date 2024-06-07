c383d69c6416b78baa0f5a461cec21b6779f61a4
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-06-07 17:21:23 +0200

    Remove update mnay

--- backend/app/services/mongo/events.py
+++ backend/app/services/mongo/events.py
@@ -214,7 +214,7 @@ async def confirm_event(
     event_model = Event.model_validate(event)
 
     # Edit the event. Note: this always confirm the event.
-    result = await mongo_db["events"].update_many(
+    result = await mongo_db["events"].update_one(
         {"project_id": project_id, "id": event_id},
         {"$set": {"confirmed": True, "source": "user"}},
     )
