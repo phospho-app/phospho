4545c514eb4e7dae64c61a2d48363696c1c25e62
Pierre-LouisBJT <plbiojout@gmail.com>
2024-06-10 15:56:30 +0200

    Made the embeddings OpenAI compatible

--- backend/app/api/v2/models/embeddings.py
+++ backend/app/api/v2/models/embeddings.py
@@ -16,11 +16,36 @@ class Embedding(BaseModel):
     )
 
 
-class EmbeddingRequest(BaseModel):
-    input: str  # The text used to generate the embedding
+class EmbeddingRequest(BaseModel, extra="allow"):
+    input: str | List[str]  # The text used to generate the embedding
     model: str  # The model used to generate the embedding (a model represent the full piepline and might be user facing, ex: embed-english-v3.0)
     org_id: Optional[str] = None  # Organization identifier, optional
     project_id: Optional[str] = None  # Project identifier, optional
     task_id: Optional[str] = (
         None  # The task id (phospho) used to generate the embedding
     )
+
+
+class EmbeddingResponseData(BaseModel):
+    object: Literal["embedding"] = "embedding"
+    embedding: List[float]  # The embeddings vector
+    index: int = 0  # The index of the embedding in the list of embeddings
+
+
+class EmbeddingUsage(BaseModel):
+    prompt_tokens: int
+    total_tokens: int  # For the embeddings api, it is equal to prompt_tokens
+
+
+class EmbeddingResponse(BaseModel):
+    """
+    Embedding response model
+    OpenAI compatible
+    """
+
+    object: Literal["list"] = "list"
+    data: List[EmbeddingResponseData]
+    model: Literal["intent-embed"] = (
+        "intent-embed"  # The model used to generate the embedding
+    )
+    usage: EmbeddingUsage  # The usage of the model, {"prompt_tokens": 8, "total_tokens": 8}
