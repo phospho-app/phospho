d3619b1d2bacb7db941bb1dbb794116cf8743a8a
Frédéric Legrand <66426745+fred3105@users.noreply.github.com>
2024-06-20 19:07:28 +0200

    Update video and fix create project

--- platform/components/navbar/create-project-button.tsx
+++ platform/components/navbar/create-project-button.tsx
@@ -6,32 +6,35 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
+import { AlertDialogTrigger } from "@radix-ui/react-alert-dialog";
 import { PlusIcon } from "lucide-react";
 import { useState } from "react";
 
 export function CreateProjectButton() {
-  const [open, setOpen] = useState(false);
+  const [openProject, setOpenProject] = useState(false);
 
   return (
-    <AlertDialog open={open} onOpenChange={setOpen}>
-      <div>
-        <HoverCard openDelay={50} closeDelay={50}>
-          <HoverCardTrigger asChild>
-            <Button variant="outline" size="icon" className="py-1 h-8 w-8">
-              <PlusIcon className="h-4 w-4" />
-            </Button>
-          </HoverCardTrigger>
-          <HoverCardContent
-            className="m-0 text-xs text-background bg-foreground"
-            align="center"
-            avoidCollisions={false}
-          >
-            <span>Create project</span>
-          </HoverCardContent>
-        </HoverCard>
-      </div>
+    <AlertDialog open={openProject} onOpenChange={setOpenProject}>
+      <AlertDialogTrigger asChild>
+        <div>
+          <HoverCard openDelay={50} closeDelay={50}>
+            <HoverCardTrigger asChild>
+              <Button variant="outline" size="icon" className="py-1 h-8 w-8">
+                <PlusIcon className="h-4 w-4" />
+              </Button>
+            </HoverCardTrigger>
+            <HoverCardContent
+              className="m-0 text-xs text-background bg-foreground"
+              align="center"
+              avoidCollisions={false}
+            >
+              <span>Create project</span>
+            </HoverCardContent>
+          </HoverCard>
+        </div>
+      </AlertDialogTrigger>
       <AlertDialogContent className="md:w-1/3">
-        <CreateProjectDialog setOpen={setOpen}></CreateProjectDialog>
+        <CreateProjectDialog setOpen={setOpenProject}></CreateProjectDialog>
       </AlertDialogContent>
     </AlertDialog>
   );
