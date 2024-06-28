510a5a7f661f5dc75e92dfd3e3e0afd2b47ce995
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-06-28 15:44:05 +0200

    Redesign settings

--- platform/app/org/settings/billing/page.tsx
+++ platform/app/org/settings/billing/page.tsx
@@ -1,13 +1,34 @@
 "use client";
 
 import Pricing from "@/components/settings/pricing";
+import TaskProgress from "@/components/settings/tasks-quota";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { authFetcher } from "@/lib/fetcher";
+import { UsageQuota } from "@/models/models";
 import { dataStateStore, navigationStateStore } from "@/store/store";
 import { useUser } from "@propelauth/nextjs/client";
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import useSWR from "swr";
+
+const StripeIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 640 512"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="mr-2 w-6 h-6 text-primary"
+    >
+      <path d="M165 144.7l-43.3 9.2-.2 142.4c0 26.3 19.8 43.3 46.1 43.3 14.6 0 25.3-2.7 31.2-5.9v-33.8c-5.7 2.3-33.7 10.5-33.7-15.7V221h33.7v-37.8h-33.7zm89.1 51.6l-2.7-13.1H213v153.2h44.3V233.3c10.5-13.8 28.2-11.1 33.9-9.3v-40.8c-6-2.1-26.7-6-37.1 13.1zm92.3-72.3l-44.6 9.5v36.2l44.6-9.5zM44.9 228.3c0-6.9 5.8-9.6 15.1-9.7 13.5 0 30.7 4.1 44.2 11.4v-41.8c-14.7-5.8-29.4-8.1-44.1-8.1-36 0-60 18.8-60 50.2 0 49.2 67.5 41.2 67.5 62.4 0 8.2-7.1 10.9-17 10.9-14.7 0-33.7-6.1-48.6-14.2v40c16.5 7.1 33.2 10.1 48.5 10.1 36.9 0 62.3-15.8 62.3-47.8 0-52.9-67.9-43.4-67.9-63.4zM640 261.6c0-45.5-22-81.4-64.2-81.4s-67.9 35.9-67.9 81.1c0 53.5 30.3 78.2 73.5 78.2 21.2 0 37.1-4.8 49.2-11.5v-33.4c-12.1 6.1-26 9.8-43.6 9.8-17.3 0-32.5-6.1-34.5-26.9h86.9c.2-2.3 .6-11.6 .6-15.9zm-87.9-16.8c0-20 12.3-28.4 23.4-28.4 10.9 0 22.5 8.4 22.5 28.4zm-112.9-64.6c-17.4 0-28.6 8.2-34.8 13.9l-2.3-11H363v204.8l44.4-9.4 .1-50.2c6.4 4.7 15.9 11.2 31.4 11.2 31.8 0 60.8-23.2 60.8-79.6 .1-51.6-29.3-79.7-60.5-79.7zm-10.6 122.5c-10.4 0-16.6-3.8-20.9-8.4l-.3-66c4.6-5.1 11-8.8 21.2-8.8 16.2 0 27.4 18.2 27.4 41.4 .1 23.9-10.9 41.8-27.4 41.8zm-126.7 33.7h44.6V183.2h-44.6z" />
+    </svg>
+  );
+};
 
 export default function Page() {
   const selectedOrgId = navigationStateStore((state) => state.selectedOrgId);
@@ -25,6 +46,9 @@ export default function Page() {
     [`/api/organizations/${selectedOrgId}/billing-portal`, accessToken],
     ([url, token]) =>
       authFetcher(url, token, "POST").then((res) => res.portal_url),
+    {
+      keepPreviousData: true,
+    },
   );
 
   const onBillingPortalClick = async () => {
@@ -38,22 +62,67 @@ export default function Page() {
     }
   };
 
+  const { data: usage }: { data: UsageQuota | null | undefined } = useSWR(
+    [`/api/organizations/${selectedOrgId}/usage-quota`, accessToken],
+    ([url, token]) => authFetcher(url, token),
+  );
+
+  const currentUsage = usage?.current_usage;
+  const maxUsage = usage?.max_usage;
+
   if (loading) {
     return <>Loading...</>;
   }
 
   return (
     <>
-      <div className="flex flex-col space-y-2">
-        {plan && plan !== "hobby" && plan !== "self-hosted" && (
-          <Button
-            variant="secondary"
-            onClick={onBillingPortalClick}
-            className="w-80"
-          >
-            Manage subscription
-          </Button>
+      <div className="space-y-2 mb-4">
+        <h2 className="text-2xl font-bold tracking-tight">Usage</h2>
+        {plan === "usage_based" && (
+          <div>
+            <p>You have currently run {currentUsage ?? "..."} detections.</p>
+          </div>
         )}
+        {plan === "pro" && (
+          <div>
+            <p>You have currently run {currentUsage ?? "..."} detections.</p>
+          </div>
+        )}
+        {plan === "hobby" && (
+          <>
+            <TaskProgress
+              currentValue={currentUsage ?? 0}
+              maxValue={maxUsage ?? 1}
+            />
+            <p>
+              You have currently run {currentUsage ?? "..."}/{maxUsage}{" "}
+              detections.
+            </p>
+            <p className="text-sm text-muted-foreground mt-2">
+              Add a payment method to enable more detections.
+            </p>
+          </>
+        )}
+      </div>
+      <div className="space-y-2 mb-8">
+        <h2 className="text-2xl font-bold tracking-tight">Plan</h2>
+        <div>
+          You plan is: <code className="bg-secondary p-1.5">{plan}</code>
+        </div>
+        {plan && plan !== "hobby" && plan !== "self-hosted" && (
+          <div className="flex flex-row space-x-2 items-center">
+            <div className="text-sm text-muted-foreground">
+              Payment, invoices, and next bill for your organization:
+            </div>
+            <Button variant="secondary" onClick={onBillingPortalClick}>
+              <StripeIcon />
+              Billing portal
+              <ExternalLink className="w-3 h-3 ml-2" />
+            </Button>
+          </div>
+        )}
+      </div>
+      <div className="space-y-2 mb-8">
         {plan === "hobby" && (
           <Pricing
             currentPlan={null}
@@ -65,7 +134,6 @@ export default function Page() {
         {plan === "usage_based" && <Pricing currentPlan="usage_based" />}
         {plan === "pro" && <Pricing currentPlan="pro" />}
         {plan === "self-hosted" && <Pricing currentPlan="self-hosted" />}
-        <div className="h-20"></div>
       </div>
     </>
   );
