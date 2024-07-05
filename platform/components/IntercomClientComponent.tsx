53ad3587ea4aad0914fb01db413d31d1c9871be1
Pierre-LouisBJT <plbiojout@gmail.com>
2024-07-05 16:25:26 +0200

    Added support for loggedin users in Intercom bot

--- platform/components/IntercomClientComponent.tsx
+++ platform/components/IntercomClientComponent.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useEffect } from "react";
+import { useUser } from "@propelauth/nextjs/client";
 
 declare global {
     interface Window {
@@ -9,11 +10,23 @@ declare global {
   }
 
 const IntercomClientComponent: React.FC = () => {
+    const { user, loading} = useUser();
     useEffect(() => {
         window.intercomSettings = {
             api_base: "https://api-iam.intercom.io",
             app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID // Replace with your Intercom app ID.
         };
+
+        if (user) {
+            console.log("User is logged in");
+            console.log("User :", user);
+            window.intercomSettings.user_id = user.userId;
+            window.intercomSettings.email = user.email;
+        }
+        else {
+            console.log("User is not logged in");
+        }
+        console.log("Intercom Settings: ", window.intercomSettings);
         if (window.Intercom) {
             window.Intercom('reattach_activator');
             window.Intercom('update', window.intercomSettings);
@@ -25,7 +38,7 @@ const IntercomClientComponent: React.FC = () => {
             intercomScript.onload = () => window.Intercom('update', window.intercomSettings);
             document.body.appendChild(intercomScript);
         }
-    }, []);
+    }, [user]);
     return null; // This component does not render anything visually.
 };
 export default IntercomClientComponent;
