741bff7e9a7fda5b7b4cc91d5fe588fb3c1c8951
flamschou <wandrille.flamant@phospho.ai>
2024-07-19 16:58:09 +0200

    style: Add graphics in frontend

--- platform/components/insights/events/event-analytics.tsx
+++ platform/components/insights/events/event-analytics.tsx
@@ -4,11 +4,13 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { authFetcher } from "@/lib/fetcher";
 import { Project } from "@/models/models";
 import { navigationStateStore } from "@/store/store";
 import { useUser } from "@propelauth/nextjs/client";
+import { Tag } from "lucide-react";
 import React from "react";
 import useSWR from "swr";
 
@@ -76,9 +78,131 @@ function EventAnalytics({ eventId }: { eventId: string }) {
 
   console.log(totalNbDetections);
 
-  return (
-    <div className="space-y-2">
-      <ComingSoon />
+  if (event?.score_range_settings?.score_type != "confidence") {
+    return (
+      <div className="space-y-2">
+        <ComingSoon />
+        <div className="mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div>
+              <Card>
+                <CardHeader>
+                  <CardDescription>Total Nb of detections</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {(totalNbDetections?.total_nb_events === undefined && (
+                    <p>...</p>
+                  )) || (
+                      <p className="text-xl">
+                        {totalNbDetections?.total_nb_events}
+                      </p>
+                    )}
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  else if (!F1Score?.f1_score) {
+    return (
+      <>
+        <Card className="bg-secondary">
+          <CardHeader>
+            <div className="flex">
+              <Tag className="mr-4 h-16 w-16 hover:text-green-500 transition-colors" />
+
+              <div className="flex flex-grow justify-between items-center">
+                <div>
+                  <CardTitle className="text-2xl font-bold tracking-tight mb-0">
+                    <div className="flex flex-row place-items-center">
+                      Unlock event metrics !
+                    </div>
+                  </CardTitle>
+                  <CardDescription className="flex justify-between flex-col text-muted-foreground space-y-0.5">
+                    <p>
+                      Give us more feedback to compute the F1-score, Precision and Recall.
+                    </p>
+                  </CardDescription>
+                </div>
+              </div>
+            </div>
+          </CardHeader>
+        </Card>
+        <div className="mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div>
+              <Card>
+                <CardHeader>
+                  <CardDescription>Total Nb of detections</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {(totalNbDetections?.total_nb_events === undefined && (
+                    <p>...</p>
+                  )) || (
+                      <p className="text-xl">
+                        {totalNbDetections?.total_nb_events}
+                      </p>
+                    )}
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div>
+              <Card>
+                <CardHeader>
+                  <CardDescription>F1 - score</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {
+                    <p>...</p>
+                  }
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div>
+              <Card>
+                <CardHeader>
+                  <CardDescription>Precision</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {
+                    <p>...</p>
+                  }
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div>
+              <Card>
+                <CardHeader>
+                  <CardDescription>Recall</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {
+                    <p>...</p>
+                  }
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  else {
+    return (
       <div className="mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div>
@@ -101,13 +225,11 @@ function EventAnalytics({ eventId }: { eventId: string }) {
                 <CardDescription>F1-score</CardDescription>
               </CardHeader>
               <CardContent>
-                {(F1Score?.f1_score === undefined && (
-                  <p>...</p>
-                )) || (
-                    <p className="text-xl">
-                      {F1Score?.f1_score}
-                    </p>
-                  )}
+                {(
+                  <p className="text-xl">
+                    {F1Score?.f1_score.toFixed(2)}
+                  </p>
+                )}
               </CardContent>
             </Card>
             <Card>
@@ -115,13 +237,11 @@ function EventAnalytics({ eventId }: { eventId: string }) {
                 <CardDescription>Precision</CardDescription>
               </CardHeader>
               <CardContent>
-                {(F1Score?.precision === undefined && (
-                  <p>...</p>
-                )) || (
-                    <p className="text-xl">
-                      {F1Score?.precision}
-                    </p>
-                  )}
+                {(
+                  <p className="text-xl">
+                    {F1Score?.precision.toFixed(2)}
+                  </p>
+                )}
               </CardContent>
             </Card>
 
@@ -130,20 +250,18 @@ function EventAnalytics({ eventId }: { eventId: string }) {
                 <CardDescription>Recall</CardDescription>
               </CardHeader>
               <CardContent>
-                {(F1Score?.recall === undefined && (
-                  <p>...</p>
-                )) || (
-                    <p className="text-xl">
-                      {F1Score?.recall}
-                    </p>
-                  )}
+                {(
+                  <p className="text-xl">
+                    {F1Score?.recall.toFixed(2)}
+                  </p>
+                )}
               </CardContent>
             </Card>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default EventAnalytics;
