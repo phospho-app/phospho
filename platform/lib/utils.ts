58cc0e3d3260095e5bc8f993aa8ada258a5e2a4a
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-08-05 17:54:56 +0200

    Readable version ids

--- platform/lib/utils.ts
+++ platform/lib/utils.ts
@@ -1,8 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 interface Session {
@@ -18,16 +18,21 @@ interface Session {
   data: {};
 }
 
-export const countObjectsPerDay = (data: Session[]): Record<string, number>  => {
-  return data.reduce((counts, item) => {
-    // Convert the Unix timestamp to a date string in 'YYYY-MM-DD' format
-    const date = new Date(Number(item.created_at) * 1000).toISOString().split("T")[0];
+export const countObjectsPerDay = (data: Session[]): Record<string, number> => {
+  return data.reduce(
+    (counts, item) => {
+      // Convert the Unix timestamp to a date string in 'YYYY-MM-DD' format
+      const date = new Date(Number(item.created_at) * 1000)
+        .toISOString()
+        .split("T")[0];
 
-    // Initialize or increment the count for each day
-    counts[date] = (counts[date] || 0) + 1;
+      // Initialize or increment the count for each day
+      counts[date] = (counts[date] || 0) + 1;
 
-    return counts;
-  }, {} as Record<string, number>);
+      return counts;
+    },
+    {} as Record<string, number>,
+  );
 };
 
 // list of 55 languages supported by langdetect
@@ -89,10 +94,98 @@ export function getLanguageLabel(isoCode: string | null): string {
     vi: "Vietnamese",
     "zh-cn": "Chinese (Simplified)",
     "zh-latn": "Chinese (Simplified)",
-    "zh-tw": "Chinese (Traditional)"
+    "zh-tw": "Chinese (Traditional)",
   };
   if (!isoCode) {
     return "Unknown";
   }
   return languageMap[isoCode.toLowerCase()] || "Unknown";
+}
+
+export function generateSlug() {
+  // Generate a random slug
+
+  function getRandomItem<T>(array: T[]): T {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function getCompactDate() {
+    const date = new Date();
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year}${month}${day}`;
+  }
+
+  const colors = [
+    "blue",
+    "green",
+    "red",
+    "neon",
+    "azure",
+    "emerald",
+    "sapphire",
+    "amber",
+    "violet",
+    "coral",
+    "indigo",
+    "teal",
+    "scarlet",
+    "jade",
+    "slate",
+    "ivory",
+    "ruby",
+    "gold",
+    "silver",
+    "bronze",
+    "copper",
+    "platinum",
+    "pearl",
+    "diamond",
+    "topaz",
+    "opal",
+    "anthracite",
+    "magenta",
+    "turquoise",
+    "lavender",
+    "rose",
+  ];
+
+  const fruits = [
+    "mango",
+    "kiwi",
+    "pineapple",
+    "dragonfruit",
+    "papaya",
+    "lychee",
+    "pomegranate",
+    "guava",
+    "fig",
+    "passionfruit",
+    "starfruit",
+    "coconut",
+    "jackfruit",
+    "banana",
+    "apple",
+    "orange",
+    "grapefruit",
+    "lemon",
+    "lime",
+    "strawberry",
+    "soursop",
+    "beaujaulais",
+    "cantaloupe",
+    "watermelon",
+    "honeydew",
+    "cucumber",
+    "tomato",
+    "potato",
+    "carrot",
+    "beet",
+  ];
+
+  const color = getRandomItem(colors);
+  const fruit = getRandomItem(fruits);
+  const date = getCompactDate();
+  return `${date}_${color}-${fruit}`;
 }
