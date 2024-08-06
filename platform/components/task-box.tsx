0e80b745f5608c9c1728b5d29e296632a054da1a
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-08-06 12:09:05 +0200

    Adds visualization of the system prompt

--- platform/components/task-box.tsx
+++ platform/components/task-box.tsx
@@ -64,10 +64,24 @@ const TaskBox = ({
         />
       </div>
       <div className="flex flex-col space-y-0.5 ">
+        {task.metadata?.system_prompt && (
+          <div className="flex justify-start pt-1">
+            <div className="flex flex-col">
+              <div className="text-muted-foreground ml-4 text-xs">
+                System Prompt:
+              </div>
+              <div className="bg-primary text-secondary min-w-[200px] rounded-lg px-2 py-1 mx-2 whitespace-pre-wrap">
+                <ReactMarkdown className="m-1">
+                  {task.metadata.system_prompt}
+                </ReactMarkdown>
+              </div>
+            </div>
+          </div>
+        )}
         <div className="flex justify-start">
           <div>
-            <div className="text-muted-foreground ml-4 text-sm">User:</div>
-            <div className="bg-secondary min-w-[200px] rounded-lg px-2 py-1 mx-2 whitespace-pre-wrap">
+            <div className="text-muted-foreground ml-4 text-xs">User:</div>
+            <div className="bg-green-500 text-secondary min-w-[200px] rounded-lg px-2 py-1 mx-2 whitespace-pre-wrap">
               {task.input && (
                 <ReactMarkdown className="m-1">{task.input}</ReactMarkdown>
               )}
@@ -77,13 +91,11 @@ const TaskBox = ({
         {task.output && (
           <div className="flex justify-start pt-1">
             <div className="flex flex-col">
-              <div className="text-muted-foreground ml-4 text-sm">
+              <div className="text-muted-foreground ml-4 text-xs">
                 Assistant:
               </div>
-              <div className="bg-green-500 text-secondary min-w-[200px] rounded-lg px-2 py-1 mx-2 whitespace-pre-wrap">
-                {task.output && (
-                  <ReactMarkdown className="m-1">{task.output}</ReactMarkdown>
-                )}
+              <div className="bg-secondary min-w-[200px] rounded-lg px-2 py-1 mx-2 whitespace-pre-wrap">
+                <ReactMarkdown className="m-1">{task.output}</ReactMarkdown>
               </div>
             </div>
           </div>
