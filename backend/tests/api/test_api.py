3ba30e6ac63c6923f68d9b021a4621abcf5edf03
Frédéric Legrand <66426745+fred3105@users.noreply.github.com>
2024-08-20 18:32:26 +0200

    fix: backend tests

--- backend/tests/api/test_api.py
+++ backend/tests/api/test_api.py
@@ -152,7 +152,7 @@ def test_create_and_update_task(
         # Flag the task
         response = client.post(
             f"/api/tasks/{task['id']}/human-eval",
-            json={"human-eval": "success"},
+            json={"human_eval": "success"},
             headers={"Authorization": f"Bearer {access_token}"},
         )
         assert response.status_code == 200, response.text
@@ -188,7 +188,7 @@ def test_create_and_update_task(
         # Flag the task
         response = client.post(
             f"/v2/tasks/{task['id']}/human-eval",
-            json={"human-eval": "failure"},
+            json={"human_eval": "failure"},
             headers={"Authorization": f"Bearer {api_key}"},
         )
         assert response.status_code == 200, response.text
