244c2d679fdac5a2dfd0fc8f391a1f3fe1ba9a92
flamschou <wandrille.flamant@phospho.ai>
2024-08-20 16:15:09 +0200

    style: Add pickaxe animation

--- platform/components/insights/clusters/clusters-cards.tsx
+++ platform/components/insights/clusters/clusters-cards.tsx
@@ -1,40 +1,26 @@
 "use client";
 
-import { TableNavigation } from "@/components/table-navigation";
 import { Card } from "@/components/ui/card";
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-} from "@/components/ui/select";
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
 import { authFetcher } from "@/lib/fetcher";
-import { formatUnixTimestampToLiteralDatetime } from "@/lib/time";
 import { Cluster, Clustering } from "@/models/models";
 import { navigationStateStore } from "@/store/store";
 import { useUser } from "@propelauth/nextjs/client";
 import {
     ColumnFiltersState,
     SortingState,
-    flexRender,
-    getCoreRowModel,
-    getFilteredRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-    useReactTable,
 } from "@tanstack/react-table";
 import { useRouter } from "next/navigation";
 import React from "react";
 import useSWR from "swr";
+import { ChevronRight, Pickaxe } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import Link from "next/link";
+import "./style.css"
 
 export function ClustersCards({
     clusterings = [],
@@ -81,6 +67,11 @@ export function ClustersCards({
         },
     );
 
+    const dataFilters = navigationStateStore((state) => state.dataFilters);
+    const setDataFilters = navigationStateStore(
+        (state) => state.setDataFilters,
+    );
+
     if (!project_id) {
         return <></>;
     }
@@ -88,34 +79,53 @@ export function ClustersCards({
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {clustersData?.map((cluster) => (
-                <Card
-                    key={cluster.id}
-                    className="rounded-lg shadow-md p-4 flex flex-col justify-between h-full"
-                >
-                    <h3 className="text-lg font-bold mb-4">{cluster.name}</h3>
-                    <p className="text-sm text-muted-foreground">
-                        {cluster.description}
-                    </p>
-                    <div className="flex justify-between items-center mt-4">
-                        <div>
-                            <span className="text-muted-foreground">Size:  </span>
-                            <span className="text-lg">
-                                {cluster.size}
-                            </span>
+
+                <Card key={cluster.id} className="rounded-lg shadow-md p-4 flex flex-col justify-between h-full">
+                    <HoverCard openDelay={0} closeDelay={0}>
+                        <div className="flex justify-between items-start space-x-2 mb-2">
+                            <h3 className="text-lg font-semibold">{cluster.name}</h3>
+                            <HoverCardTrigger asChild>
+                                <div className="text-xl bg-primary text-secondary rounded-tr-lg px-3">{cluster.size}</div>
+                            </HoverCardTrigger>
                         </div>
-                        <button
-                            className="text-muted-foreground"
-                            onClick={() => {
-                                router.push(
-                                    `/org/insights/clusters/${cluster.id}`
-                                );
-                            }}
-                        >
-                            View
-                        </button>
+                        <HoverCardContent className="text-secondary bg-primary text-xs">
+                            <p>Cluster size</p>
+                        </HoverCardContent>
+                    </HoverCard>
+                    <p className="text-sm text-muted-foreground mb-4">{cluster.description}</p>
+                    <div className="mt-auto pt-2 flex justify-end space-x-2">
+                        {cluster.size > 5 && (
+                            <Button
+                                className="pickaxe-button"
+                                variant="outline"
+                                size="sm"
+                                onClick={(mouseEvent) => {
+                                    mouseEvent.stopPropagation();
+                                    setSheetOpen(true);
+                                    setDataFilters({
+                                        ...dataFilters,
+                                        clustering_id: cluster.clustering_id,
+                                        clusters_ids: [cluster.id],
+                                    });
+                                }}
+                            >
+                                Break down
+                                <Pickaxe className="w-6 h-6 ml-1 pickaxe-animation" />
+                            </Button>
+                        )}
+                        <Link href={`/org/insights/clusters/${encodeURIComponent(cluster.id)}`}>
+                            <Button
+                                variant="secondary"
+                                size="sm"
+                            >
+                                Explore
+                                <ChevronRight className="w-4 h-4 ml-1" />
+                            </Button>
+                        </Link>
                     </div>
                 </Card>
-            ))}
-        </div>
+            ))
+            }
+        </div >
     );
 }
