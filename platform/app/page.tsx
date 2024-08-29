5d8a1a8990554c4b3f37ee3a2c546d60260928f7
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-08-29 12:00:15 +0200

    Redirect to authenticate if not connected

--- platform/app/page.tsx
+++ platform/app/page.tsx
@@ -2,11 +2,19 @@
 
 import FetchOrgProject from "@/components/fetch-data/fetch-org-project";
 import FullPageLoader from "@/components/full-page-loader";
+import { useUser } from "@propelauth/nextjs/client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Page: React.FC = () => {
   if (process.env.NODE_ENV === "production") {
     console.log = function () {};
+  }
+
+  const { user, loading } = useUser();
+  const router = useRouter();
+  if (!loading && !user) {
+    router.push("/authenticate");
   }
 
   return (
