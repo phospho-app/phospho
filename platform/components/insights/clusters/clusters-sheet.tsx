16e50ab1dbb2cae031aa85098be0f4f21b9fe5b2
flamschou <wandrille.flamant@phospho.ai>
2024-09-06 11:02:00 +0200

    refactor: Remove unused imports in clusters-sheet.tsx

--- platform/components/insights/clusters/clusters-sheet.tsx
+++ platform/components/insights/clusters/clusters-sheet.tsx
@@ -47,8 +47,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useUser } from "@propelauth/nextjs/client";
 import { Separator } from "@radix-ui/react-dropdown-menu";
 import { QuestionMarkIcon } from "@radix-ui/react-icons";
-import cluster from "cluster";
-import { nb } from "date-fns/locale";
 import { ChevronRight, Sparkles, TestTubeDiagonal } from "lucide-react";
 import React from "react";
 import { useEffect, useState } from "react";
@@ -413,7 +411,7 @@ const RunClusters = ({
                       render={({ field }) => (
                         <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
                           <FormLabel className="space-y-1 leading-none">
-                            Detect outliers (coming soon)
+                            Detect outliers
                           </FormLabel>
                           <FormControl>
                             <Checkbox
