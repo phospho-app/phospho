1bec2638c1deec9731cbdd33ed2ffc3c8aa842cf
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-09-09 21:34:03 +0200

    Adds more error code and reset state store

--- platform/app/error.tsx
+++ platform/app/error.tsx
@@ -1,14 +1,29 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { navigationStateStore } from "@/store/store";
 
-export default function Error({ reset }: { reset: () => void }) {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   return (
     <div>
       <h2>
         Something went wrong! Please make sure your browser is up to date.
       </h2>
-      <Button onClick={() => reset()}>Reload</Button>
+      <code>{error.message}</code>
+      <Button
+        onClick={() => {
+          navigationStateStore.persist.clearStorage();
+          reset();
+        }}
+      >
+        Reload
+      </Button>
     </div>
   );
 }
--- platform/app/org/error.tsx
+++ platform/app/org/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { navigationStateStore } from "@/store/store";
 
 export default function Error({
   error,
@@ -14,7 +15,15 @@ export default function Error({
       <h2>
         Something went wrong! Please make sure your browser is up to date.
       </h2>
-      <Button onClick={() => reset()}>Reload</Button>
+      <code>{error.message}</code>
+      <Button
+        onClick={() => {
+          navigationStateStore.persist.clearStorage();
+          reset();
+        }}
+      >
+        Reload
+      </Button>
     </div>
   );
 }
