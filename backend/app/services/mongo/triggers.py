c28f01840d2a7b90656a4deed0ea0490d086a040
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-09-11 19:33:52 +0200

    Types

--- backend/app/services/mongo/triggers.py
+++ backend/app/services/mongo/triggers.py
@@ -24,25 +24,30 @@ def aggregate_tasks_into_sessions(tasks: List[Task], project_id: str) -> List[Se
                 sessions[task.session_id].created_at = task.created_at
                 sessions[task.session_id].preview = task.preview()
             sessions[task.session_id].session_length += 1
-            sessions[task.session_id].tasks += [task]
+            session_tasks = sessions[task.session_id].tasks
+            if session_tasks is not None:
+                session_tasks.append(task)
+            else:
+                sessions[task.session_id].tasks = [task]
 
     validated_sessions: List[Session] = []
     # Process each session
     for sessions_id, session in sessions.items():
+        if session.tasks is None:
+            continue
         try:
-            validated_session = session
-            validated_session.metadata = {
+            session.metadata = {
                 "total_tokens": sum(
                     [
                         task.metadata["total_tokens"]
-                        for task in validated_session.tasks
+                        for task in session.tasks
                         if task.metadata is not None and "total_tokens" in task.metadata
                     ]
                 ),
                 "prompt_tokens": sum(
                     [
                         task.metadata["prompt_tokens"]
-                        for task in validated_session.tasks
+                        for task in session.tasks
                         if task.metadata is not None
                         and "prompt_tokens" in task.metadata
                     ]
@@ -50,41 +55,39 @@ def aggregate_tasks_into_sessions(tasks: List[Task], project_id: str) -> List[Se
                 "completion_tokens": sum(
                     [
                         task.metadata["completion_tokens"]
-                        for task in validated_session.tasks
+                        for task in session.tasks
                         if task.metadata is not None
                         and "completion_tokens" in task.metadata
                     ]
                 ),
             }
-            validated_session.stats = SessionStats(
+            session.stats = SessionStats(
                 avg_magnitude_score=sum(
                     [
                         task.metadata["magnitude_score"]
-                        for task in validated_session.tasks
+                        for task in session.tasks
                         if task.metadata is not None
                         and "magnitude_score" in task.metadata
                     ]
                 )
-                / validated_session.session_length,
+                / session.session_length,
                 avg_sentiment_score=sum(
                     [
                         task.metadata["sentiment_score"]
-                        for task in validated_session.tasks
+                        for task in session.tasks
                         if task.metadata is not None
                         and "sentiment_score" in task.metadata
                     ]
                 )
-                / validated_session.session_length,
-                most_common_flag=get_most_common(
-                    [task.flag for task in validated_session.tasks]
-                ),
+                / session.session_length,
+                most_common_flag=get_most_common([task.flag for task in session.tasks]),
                 most_common_language=get_most_common(
-                    [task.language for task in validated_session.tasks]
+                    [task.language for task in session.tasks]
                 ),
                 most_common_sentiment_label=get_most_common(
                     [
                         task.metadata["sentiment_label"]
-                        for task in validated_session.tasks
+                        for task in session.tasks
                         if task.metadata is not None
                         and "sentiment_label" in task.metadata
                     ]
@@ -92,14 +95,14 @@ def aggregate_tasks_into_sessions(tasks: List[Task], project_id: str) -> List[Se
                 human_eval=get_most_common(
                     [
                         task.metadata["human_eval"]
-                        for task in validated_session.tasks
+                        for task in session.tasks
                         if task.metadata is not None and "human_eval" in task.metadata
                     ]
                 ),
             )
-            validated_session.tasks = []
-            logger.debug(f"Created session: {validated_session.id}")
-            validated_sessions.append(validated_session)
+            session.tasks = []
+            logger.debug(f"Created session: {session.id}")
+            validated_sessions.append(session)
         except Exception as e:
             logger.warning(f"Error creating session: {e}")
             continue
