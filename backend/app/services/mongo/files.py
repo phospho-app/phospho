83b18332e99466d92a4f1864d42ee1f085779ce3
flamschou <wandrille.flamant@phospho.ai>
2024-09-12 12:02:50 +0200

    refactor: Remove unused files and endpoints

--- backend/app/services/mongo/files.py
+++ backend/app/services/mongo/files.py
@@ -1,89 +1,15 @@
-from typing import List
-
 from app.core.constants import (
     RESERVED_CATEGORY_METADATA_FIELDS,
     RESERVED_NUMBER_METADATA_FIELDS,
 )
 import pandas as pd
 from app.api.v2.models.log import LogEvent
-from app.core.config import CSV_UPLOAD_MAX_ROWS
-from app.db.models import DatasetRow
-from app.db.mongo import get_mongo_db
 from app.security.authorization import get_quota
 from app.services.mongo.emails import send_quota_exceeded_email
 from app.services.mongo.extractor import ExtractorClient
 from app.utils import generate_uuid
 from loguru import logger
 from pydantic import ValidationError
-
-
-async def process_csv_file_as_df(
-    file_id: str,
-    file_name: str,
-    df: pd.DataFrame,
-    org_id: str,
-    max_rows: int = CSV_UPLOAD_MAX_ROWS,
-    correct_columns: List[str] = ["text", "label", "label_text"],
-):
-    """
-    Process a csv file as a pandas dataframe and store it in the database.
-
-    We parse csv, and cast input, output, session_id, task_id, user_id, version_id as string.
-    """
-
-    # Check that the csv has less than max_rows
-    if len(df) > max_rows:
-        logger.error(
-            f"The file {file_id} of org_id {org_id} has more than {max_rows} rows. It won't be processed."
-        )
-        # Only get the first max_rows
-        df = df.head(max_rows)
-
-    # Check that the correct_columns are in the dataframe columns
-    if not all([col in df.columns for col in correct_columns]):
-        logger.error(
-            f"The file {file_id} of org_id {org_id} does not have the correct columns. It won't be processed."
-        )
-        return
-
-    # only keep the correct columns
-    df = df[correct_columns]
-
-    # Add the file_id to the dataframe
-    df["file_id"] = file_id
-    df["file_name"] = file_name
-    df["org_id"] = org_id
-
-    # Convert dataframe to list of dict
-    records = df.to_dict(orient="records")
-
-    # Validate each record with DatasetRow model
-    valid_records: List[dict] = []
-    for record in records:
-        logger.debug(f"version_id: {record.get('version_id')}")
-        try:
-            for metadata_field in RESERVED_CATEGORY_METADATA_FIELDS:
-                if record.get(metadata_field) is not None and not isinstance(
-                    record[metadata_field], str
-                ):
-                    record[metadata_field] = str(record[metadata_field])
-            for metadata_field in RESERVED_NUMBER_METADATA_FIELDS:
-                if record.get(metadata_field).isdigit():
-                    record[metadata_field] = float(record[metadata_field])
-            valid_record = DatasetRow(**record).model_dump()
-            valid_records.append(valid_record)
-        except ValidationError as e:
-            logger.warning(f"Validation error for record {record}: {e}")
-            continue
-
-    mongo_db = await get_mongo_db()
-
-    # Insert the valid records in the database collection datasets
-    await mongo_db.datasets.insert_many(valid_records)
-
-    logger.info(
-        f"File {file_id} processed successfully, added {len(valid_records)} records."
-    )
 
 
 async def process_file_upload_into_log_events(
