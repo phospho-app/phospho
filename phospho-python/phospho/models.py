e3d8480993489bb8beed553025f8c54ed2403758
flamschou <wandrille.flamant@phospho.ai>
2024-10-10 16:47:45 +0200

    feat: Add task_position field to FlattenedTask model

--- phospho-python/phospho/models.py
+++ phospho-python/phospho/models.py
@@ -424,6 +424,7 @@ class FlattenedTask(BaseModel, extra="allow"):
     task_eval_source: Optional[str] = None
     task_eval_at: Optional[int] = None
     task_created_at: Optional[int] = None
+    task_position: Optional[int] = None
     session_id: Optional[str] = None
     session_length: Optional[int] = None
     event_name: Optional[str] = None
