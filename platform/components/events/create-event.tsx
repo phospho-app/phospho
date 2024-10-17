3dd8c8ad0d90198a6b9ec0360c404eec629f65b6
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-10-17 17:16:36 +0200

    Rename

--- platform/components/events/create-event.tsx
+++ platform/components/events/create-event.tsx
@@ -235,8 +235,10 @@ export default function CreateEvent({
     // On purpose, we do not pass the job_id, so a new job object will be created for this event
     selectedProject.settings.events[values.event_name] = {
       id: eventToEdit?.id,
-      version_id:
-        eventToEdit?.version_id !== undefined ? eventToEdit?.version_id + 1 : 1,
+      event_version_id:
+        eventToEdit?.event_version_id !== undefined
+          ? eventToEdit?.event_version_id + 1
+          : 1,
       project_id: selectedProject.id,
       org_id: selectedProject.org_id,
       event_name: values.event_name,
