026c27892b7e83f33c703d12f4fe72a763be5b2c
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-11-26 09:43:37 +0100

    Update exempted org list

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -147,13 +147,7 @@ else:
 # These orgs are exempted from the quota
 EXEMPTED_ORG_IDS = [
     "13b5f728-21a5-481d-82fa-0241ca0e07b9",  # phospho
-    "bb46a507-19db-4e11-bf26-6bd7cdc8dcdd",  # e
     "a5724a02-a243-4025-9b34-080f40818a31",  # m
-    "144df1a7-40f6-4c8d-a0a2-9ed010c1a142",  # v
-    "3bf3f4b0-2ef7-47f7-a043-d96e9f5a3d7e",  # st
-    "2fdbcf01-eb52-4747-bb14-b66621973e8f",  # sa
-    "5a3d67ab-231c-4ad1-adba-84b6842668ad",  # sa (a)
-    "7e8f6db2-3b6b-4bf6-84ee-3f226b81e43d",  # di
 ]
 
 
--- platform/app/org/layout.tsx
+++ platform/app/org/layout.tsx
@@ -35,14 +35,7 @@ export default function OrgLayout({ children }: { children: React.ReactNode }) {
     // Some orgs are not subject to the blockwall
     const EXEMPTED_ORG_IDS = [
       "13b5f728-21a5-481d-82fa-0241ca0e07b9", // phospho
-      "bb46a507-19db-4e11-bf26-6bd7cdc8dcdd", // e
       "a5724a02-a243-4025-9b34-080f40818a31", // m
-      "144df1a7-40f6-4c8d-a0a2-9ed010c1a142", // v
-      "3bf3f4b0-2ef7-47f7-a043-d96e9f5a3d7e", // st
-      "8e530a71-8739-450a-844a-5a6430067f9a", // y
-      "2fdbcf01-eb52-4747-bb14-b66621973e8f", // sa
-      "5a3d67ab-231c-4ad1-adba-84b6842668ad", // sa (a)
-      "7e8f6db2-3b6b-4bf6-84ee-3f226b81e43d", // di
     ];
     if (!EXEMPTED_ORG_IDS.includes(selectedOrgId)) {
       // Uncomment this to enable the blockwall
